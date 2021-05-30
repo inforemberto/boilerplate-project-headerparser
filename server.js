4a6065dcf51e9d9f69cfeec90f4f0e9107505e31
inforemberto <>
2021-05-30 01:31:43 +0000

    header endpoint

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get('Accept-Language'), 
+    software: req.get('User-Agent')
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
